bdd590891cf59ec516fd80619ee268b4fc6f77e9
Наран <naranmonhbaatar@gamil.com>
2024-08-18 14:44:37 +0800

    Тоглогчийн ээлжийг солидог болгох

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийн хадгалах хувьсагчийг хадгалах хувьсагч, нэг-р тоглогчийг 0, хоёрдугаар тоглогчийг 1
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -26,10 +26,43 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+//Roll Dice ажиллагаатай болгох eventListener
 document.querySelector(".btn-roll").addEventListener("click", function () {
+    // Шоонд зориулж 1 - 6 хүртлэх санамсаргүй тоог гаргаж авах
     var diceRandNumber = Math.floor(Math.random() * 6) + 1;
+
+    //Шооны зургийг веб дээр гаргаж ирнэ
     diceDom.style.display = "block";
+
+    //Буусан санамсаргүй тоонд харгалзах зургийг үзүүлэх
     diceDom.src = "dice-" + diceRandNumber + ".png";
 
-    alert("Шоог буулаа : " + diceRandNumber);
+    // Буусан тоо нь нэгээс ялгаатай бол идэвхтэй тоглогчийн тоог нэмэгдүүлнэ
+    if (diceRandNumber != 1) {
+        // 1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө
+        roundScore = roundScore + diceRandNumber;
+        document.getElementById("current-" + activePlayer).textContent =
+            roundScore;
+    } else {
+        //1 буусан тул тоглогчийн ээлжийг энд сольж өгнө
+
+        //Энэ тоглогчийн ээлжиндээ цугуулсан оноог 0 болгоно
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+
+        activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        //Улаан цэгийг шилжүүлэх
+        document.querySelector(".player-0-panel").classList.toggle("active"); //Хэрэв active класс байвал устгаж, байхгүй бол нэмнэ
+        document.querySelector(".player-1-panel").classList.toggle("active"); //Хэрэв active класс байвал устгаж, байхгүй бол нэмнэ
+
+        //Шоог түр алга болгох
+        diceDom.style.display = "none";
+
+        // if (activePlayer == 0) {
+        //     activePlayer = 1;
+        // } else {
+        //     activePlayer = 0;
+        // }
+    }
 });
