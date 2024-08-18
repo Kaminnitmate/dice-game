f7ccdd651800967ab236d78196edb2b3d7b9a1ec
Наран <naranmonhbaatar@gamil.com>
2024-08-18 13:50:26 +0800

    note.txt файл устгасан

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Шооны буусан тоог хадгалах
-var dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+var diceRandNumber = Math.floor(Math.random() * 6) + 1;
+console.log(diceRandNumber);
 
 // <div class="player-score" id="score-0">43</div>
 // document.querySelector("#score-0").innerHTML = dice;
@@ -21,10 +21,10 @@ console.log(dice);
 //                        оруулахдаа ашиглана
 
 //Программ эхлэхэд бэлтгье
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".dice").style.display = "none";
